c7ac54e0250be38fe33549729bf22a6660bfb2cb
melnikov-vlad <melnikovvladislavzzz@gmail.com>
2022-11-08 01:16:34 +0200

    kursach_08.11.22

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
+    <div class="main_wrapper">
+      <div class="background_wrapper">
+        <div>
+          <h1 class="hero_text">Великий світ Ріка та Морті</h1>
+        </div>
+        <label for="search">
+          <div>
+            <h2 class="hero_task_text">Давай число від 1 до 826 і подивимося який персонаж тобі випаде</h2>
+          </div>
+          <input v-model="numberPerson" class="inp_search" type="text" name="findphrases" id="search" placeholder="Вводи число">
+        </label>
+        <br>
+        <button class="btn_search" @click="searchNumberPerson">Почнемо пошук</button>
+        <button class="btn_search" @click="">Випадкова персонаж</button>
+        <!-- if we have single person -->
+        <div class="main_img" v-if="persone.name">
+          <img class="img_found" :src="persone.image" :alt="persone.name">
+          <ul class="img_info_found">
+            <li>
+              <h3>Ім'я: {{ persone.name }}</h3>
+            </li>
+            <li>
+              <h4>Стать: {{ persone.gender }}</h4>
+            </li>
+            <li>
+              <h5>Життєвий статус: {{ persone.status }}</h5>
+            </li>
+          </ul>
+        
+        </div>
+        <!-- if we have a list of the persones -->
+        <div v-if="persones.length && !persone.name" v-for="onePers in persones" :key="persones.id">
+          <img :src="onePers.image" :alt="onePers.name">
+          <h3>{{ onePers.name }}</h3>
+        </div>
+        <!-- <div>
+                                  <img :src="persones.image" :alt="persones.name">
+                                  <h3>{{ persones.name }}</h3>
+                              </div> -->
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
+const URL = 'https://rickandmortyapi.com/api/character/';
 export default {
-  name: 'HomeView',
+  name: 'bad_page',
   components: {
-    
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      persones: [],
+      persone: {},
+      numberPerson: '',
+
+    }
+  },
+  props: {
+    type: String,
+    default: '',
+  },
+  async mounted() {
+    // this.searchAllPerson();
+  },
+  methods: {
+    async searchNumberPerson() {
+      const res = await fetch(URL + this.numberPerson);
+      const persone = await res.json();
+      this.persone = persone;
+    },
+    async searchAllPerson() {
+      const res = await fetch(URL);
+      const persones = await res.json();
+      this.persones = persones.results;
+    },
+    // async randomNumber() {
+    //   min = Math.ceil(1);
+    //   max = Math.floor(826);
+    //   return Math.floor(Math.random() * (max - min + 1) + min);
+    //   console.log(min);
+    // },
+    // async randomSearch() {
+    //   const res = await fetch(URL + randomNumber());
+    //   const persone = await res.json();
+    //   this.persone = persone;
+    // }
   }
 }
 </script>
